de74bb4b64f98acd5caedfcd8fc15aaa00268974
github ginasekhar <github ginasekhar@yahoo.com>
2019-12-04 12:33:19 -0500

    Change textarea formatting

--- app/models/practice_log.rb
+++ app/models/practice_log.rb
@@ -4,12 +4,8 @@ class PracticeLog < ActiveRecord::Base
     validates :date, presence: true
     validates :practice_minutes, presence: true
     validates :practice_minutes, numericality: { greater_than_or_equal_to: 10, less_than_or_equal_to: 600}
- 
-    #
-  
- 
+
     def log_date_in_range
-      binding.pry
       if !((Date.today - 365)..Date.today).include?(date)
         errors.add(:date, "must be within the past year")
       end
