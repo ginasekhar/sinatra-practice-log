f1ed497f16cdb2d20fddfda1f233a4a985d576a0
github ginasekhar <github ginasekhar@yahoo.com>
2019-12-04 12:05:26 -0500

    Add Date range validation on practice logs and notes formatting

--- app/models/practice_log.rb
+++ app/models/practice_log.rb
@@ -1,18 +1,17 @@
 class PracticeLog < ActiveRecord::Base
     belongs_to :student
-
+    validate :log_date_in_range
     validates :date, presence: true
-    #validates :date { after: Proc.new { Time.now - 1.year}, before_or_equal_to: Time.now }
     validates :practice_minutes, presence: true
     validates :practice_minutes, numericality: { greater_than_or_equal_to: 10, less_than_or_equal_to: 600}
-    #validates :practice_minutes, numericality: { less_than_or_equal_to: 600  }
-    
-    #validate :log_date_in_range,
+ 
+    #
   
  
-  def log_date_in_range
-    if date.valid_date? && date > Date.today
-      errors.add(:date, "can't be in the future")
+    def log_date_in_range
+      binding.pry
+      if !((Date.today - 365)..Date.today).include?(date)
+        errors.add(:date, "must be within the past year")
+      end
     end
-  end
   end
